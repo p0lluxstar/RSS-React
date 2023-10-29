9b9c9dad1c3df0674dcef135801845be3aad06d8
p0lluxstar <web-registra@yandex.ru>
2023-10-29 14:24:08 +0300

    feat: add loading pokemons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,34 +6,150 @@ interface Props {}
 
 interface State {
   inputValue: string;
-  namePokemon: string;
+  dataPokemon: Array<{
+    namePokemon: string;
+    typePokemon: string;
+    urlImg: string;
+    err: boolean;
+  }>;
+}
+
+interface manyPokemons {
+  results: [
+    {
+      name: string;
+      url: string;
+    },
+  ];
 }
 
 interface DataPokemon {
   name: string;
+  types: [
+    {
+      type: {
+        name: string;
+      };
+    },
+  ];
+  sprites: {
+    other: {
+      dream_world: {
+        front_default: string;
+      };
+    };
+  };
 }
 
 class Header extends React.Component<Props, State> {
+  placeholder: string;
+
   constructor(props: Props) {
     super(props);
-    this.state = {
-      inputValue: '',
-      namePokemon: '',
-    };
+    (this.placeholder = 'Enter pokemon name'),
+      (this.state = {
+        inputValue: '',
+        dataPokemon: [
+          {
+            namePokemon: '',
+            typePokemon: '',
+            urlImg: '',
+            err: false,
+          },
+        ],
+      });
+    this.fetchFirstPageLoad = this.fetchFirstPageLoad.bind(this);
+    setTimeout(this.fetchFirstPageLoad, 100);
     this.fetchSearch = this.fetchSearch.bind(this);
   }
 
+  fetchFirstPageLoad() {
+    const newArr: Array<{
+      pokemonName: string;
+      pokemonType: string;
+      urlImg: string;
+      err: boolean;
+    }> = [];
+    fetch(`https://pokeapi.co/api/v2/pokemon/`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data: manyPokemons) => {
+        data.results.forEach((e) => {
+          fetch(e.url)
+            .then((response) => {
+              return response.json();
+            })
+            .then((data: DataPokemon) =>
+              newArr.push({
+                pokemonName: data.name,
+                pokemonType: data.types[0].type.name,
+                urlImg: data.sprites.other.dream_world.front_default,
+                err: false,
+              })
+            );
+        });
+        const arrManyPokmon = () => {
+          console.log(newArr);
+          this.setState({
+            dataPokemon: [
+              {
+                namePokemon: newArr[0].pokemonName,
+                urlImg: newArr[0].urlImg,
+                typePokemon: newArr[0].pokemonType,
+                err: false,
+              },
+              {
+                namePokemon: newArr[1].pokemonName,
+                urlImg: newArr[1].urlImg,
+                typePokemon: newArr[1].pokemonType,
+                err: false,
+              },
+              {
+                namePokemon: newArr[2].pokemonName,
+                urlImg: newArr[2].urlImg,
+                typePokemon: newArr[2].pokemonType,
+                err: false,
+              },
+            ],
+          });
+        };
+        setTimeout(arrManyPokmon, 300);
+      })
+      .catch(() => {});
+  }
+
   fetchSearch() {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${this.state.inputValue}`)
+    this.placeholder = 'Enter pokemon name';
+    fetch(
+      `https://pokeapi.co/api/v2/pokemon/${this.state.inputValue.toLowerCase()}`
+    )
       .then((response) => {
         return response.json();
       })
       .then((data: DataPokemon) => {
-        console.log(data);
-        this.setState({ namePokemon: data.name });
+        this.setState({
+          dataPokemon: [
+            {
+              namePokemon: data.name,
+              urlImg: data.sprites.other.dream_world.front_default,
+              typePokemon: data.types[0].type.name,
+              err: false,
+            },
+          ],
+        });
       })
       .catch(() => {
-        this.setState({ namePokemon: 'There is no Pokemon with such a name.' });
+        this.setState({
+          dataPokemon: [
+            {
+              namePokemon: '',
+              typePokemon: '',
+              urlImg: '',
+              err: true,
+            },
+          ],
+        });
       });
   }
 
@@ -44,13 +160,13 @@ class Header extends React.Component<Props, State> {
           <input
             id="search"
             type="text"
-            placeholder="Enter pokemon name"
+            placeholder={this.placeholder}
             value={this.state.inputValue}
             onChange={(e) => this.setState({ inputValue: e.target.value })}
           ></input>
           <button onClick={this.fetchSearch}>Search</button>
         </header>
-        <Main namePokemon={this.state.namePokemon} />
+        <Main dataArray={this.state.dataPokemon} />
       </>
     );
   }
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,19 +2,60 @@ import React from 'react';
 import styles from './Main.module.css';
 
 interface Props {
-  namePokemon: string;
+  dataArray: Array<{
+    namePokemon: string;
+    typePokemon: string;
+    urlImg: string;
+    err: boolean;
+  }>;
 }
 
 class Main extends React.Component<Props> {
   constructor(props: Props) {
     super(props);
   }
+
+  showPokemon() {
+    if (this.props.dataArray[0].err) {
+      return (
+        <p className={styles.errName}>There is no Pokimon with that name!</p>
+      );
+    }
+    if (this.props.dataArray[0].namePokemon === '') {
+      return <p className={styles.errName}>Loading...</p>;
+    } else if (this.props.dataArray.length === 1) {
+      return (
+        <div className={styles.pokemon}>
+          <p>Name pokemon: {this.props.dataArray[0].namePokemon}</p>
+          <p>Type pokemon: {this.props.dataArray[0].typePokemon}</p>
+          <img src={this.props.dataArray[0].urlImg} />
+        </div>
+      );
+    } else {
+      return (
+        <>
+          <div className={styles.pokemon}>
+            <p>Name pokemon: {this.props.dataArray[0].namePokemon}</p>
+            <p>Type pokemon: {this.props.dataArray[0].typePokemon}</p>
+            <img src={this.props.dataArray[0].urlImg} />
+          </div>
+          <div className={styles.pokemon}>
+            <p>Name pokemon: {this.props.dataArray[1].namePokemon}</p>
+            <p>Type pokemon: {this.props.dataArray[1].typePokemon}</p>
+            <img src={this.props.dataArray[1].urlImg} />
+          </div>
+          <div className={styles.pokemon}>
+            <p>Name pokemon: {this.props.dataArray[2].namePokemon}</p>
+            <p>Type pokemon: {this.props.dataArray[2].typePokemon}</p>
+            <img src={this.props.dataArray[2].urlImg} />
+          </div>
+        </>
+      );
+    }
+  }
+
   render() {
-    return (
-      <main className={styles.main}>
-        <div>{this.props.namePokemon}</div>
-      </main>
-    );
+    return <main className={styles.main}>{this.showPokemon()}</main>;
   }
 }
 
