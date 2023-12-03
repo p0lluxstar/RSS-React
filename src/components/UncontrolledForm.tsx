fd7cacd4b4335b56e58c19605a1faa6e900b1b20
p0lluxstar <web-registra@yandex.ru>
2023-12-03 14:10:46 +0300

    feat: add validation uncontrolled form

--- src/components/UncontrolledForm.tsx
+++ src/components/UncontrolledForm.tsx
@@ -1,21 +1,59 @@
 import styles from './UncontrolledForm.module.css';
 import { useNavigate } from 'react-router-dom';
-import { useRef } from 'react';
-import { LoginSchema } from '../utils/yup';
+import { useState, useRef } from 'react';
+import { nameSchema, ageSchema, emailSchema } from '../utils/yup';
 
 const UncontrolledForm = () => {
+  const [nameError, setNameError] = useState('');
+  const [isEnteredName, setIsEnteredName] = useState(false);
+
+  const [ageError, setAgeError] = useState('');
+  const [isEnteredAge, setIsEnteredAge] = useState(false);
+
+  const [emailError, setEmailError] = useState('');
+  const [isEnteredEmail, setIsEnteredEmail] = useState(false);
+
   const nameInputRef = useRef<HTMLInputElement>(null);
+  const ageInputRef = useRef<HTMLInputElement>(null);
+  const emailInputRef = useRef<HTMLInputElement>(null);
+
   const navigate = useNavigate();
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
     const nameValue = { name: nameInputRef.current?.value };
+    const ageValue = { age: ageInputRef.current?.value };
+    const emailValue = { email: emailInputRef.current?.value };
 
     try {
-      await LoginSchema.validate(nameValue);
-      navigate('/');
+      await nameSchema.validate(nameValue);
+      setIsEnteredName(false);
     } catch (validationError) {
-      alert(validationError);
+      setIsEnteredName(true);
+      setNameError((validationError as Error).message);
+      nameValue.name = undefined;
+    }
+
+    try {
+      await ageSchema.validate(ageValue);
+      setIsEnteredAge(false);
+    } catch (validationError) {
+      setIsEnteredAge(true);
+      setAgeError((validationError as Error).message);
+      ageValue.age = undefined;
+    }
+
+    try {
+      await emailSchema.validate(emailValue);
+      setIsEnteredEmail(false);
+    } catch (validationError) {
+      setIsEnteredEmail(true);
+      setEmailError((validationError as Error).message);
+      emailValue.email = undefined;
+    }
+
+    if (nameValue.name && ageValue.age && emailValue.email) {
+      navigate('/');
     }
   };
 
@@ -24,8 +62,11 @@ const UncontrolledForm = () => {
       <h1>Uncontrolled Form</h1>
       <form className={styles.uf} onSubmit={handleSubmit}>
         <input type="text" ref={nameInputRef} placeholder="Name" />
-        <input type="number" placeholder="Age" />
-        <input type="text" placeholder="Email" />
+        {isEnteredName && <p className={styles.error}>{nameError}</p>}
+        <input type="number" ref={ageInputRef} placeholder="Age" />
+        {isEnteredAge && <p className={styles.error}>{ageError}</p>}
+        <input type="text" ref={emailInputRef} placeholder="Email" />
+        {isEnteredEmail && <p className={styles.error}>{emailError}</p>}
         <div className={styles.btn}>
           <button type="submit">Submit</button>
         </div>
--- src/utils/yup/index.tsx
+++ src/utils/yup/index.tsx
@@ -1,10 +1,25 @@
 import * as yup from 'yup';
 
-export const LoginSchema = yup
+export const nameSchema = yup
   .object({
     name: yup
       .string()
       .required('The name is required')
       .matches(/^[A-ZА-Я]/, 'The name must begin with a capital letter'),
+  })
+  .required();
+
+export const ageSchema = yup
+  .object({
+    age: yup.string().required('The age is required'),
+  })
+  .required();
+
+export const emailSchema = yup
+  .object({
+    email: yup
+      .string()
+      .required('The email is required')
+      .email('The email must contain the "@" and the domain name'),
   })
   .required();
