ed2b149bf590df54cd5a35bff455eb23f0a6529b
p0lluxstar <web-registra@yandex.ru>
2024-08-06 13:25:51 +0300

    init: @vitest/coverage-v8

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './vitest.setup.ts',
     coverage: {
-      provider: 'istanbul',
+      provider: 'v8',
       reporter: ['text', 'html'],
       exclude: ['node_modules/**'],
       include: ['src/**'],
